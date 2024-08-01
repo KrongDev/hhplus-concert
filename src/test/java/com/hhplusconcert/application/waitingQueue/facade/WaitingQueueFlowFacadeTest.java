0fe8f189553d741f4edac15db7189ff4fe99b1ee
KrongDev <leegeondev@gmail.com>
2024-08-02 01:41:58 +0900

    fix: TestCode Error

--- src/test/java/com/hhplusconcert/application/waitingQueue/facade/WaitingQueueFlowFacadeTest.java
+++ src/test/java/com/hhplusconcert/application/waitingQueue/facade/WaitingQueueFlowFacadeTest.java
@@ -1,10 +1,13 @@
 package com.hhplusconcert.application.waitingQueue.facade;
 
 import com.hhplusconcert.common.TruncateTableComponent;
+import com.hhplusconcert.domain.common.exception.model.CustomGlobalException;
 import com.hhplusconcert.infra.waitingToken.orm.WaitingTokenJpaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -15,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 @ActiveProfiles("test")
 class WaitingQueueFlowFacadeTest {
+    private static final Logger log = LoggerFactory.getLogger(WaitingQueueFlowFacadeTest.class);
     //
     @Autowired
     private WaitingQueueFlowFacade waitingQueueFlowFacade;
@@ -38,7 +42,11 @@ class WaitingQueueFlowFacadeTest {
         String userId = "test_userId";
         String seriesId = "test_seriesId";
         for(int i = 0; i < 55; i++) {
-            waitingQueueFlowFacade.joinQueue(userId + i, seriesId);
+            try {
+                waitingQueueFlowFacade.joinQueue(userId + i, seriesId);
+            }catch (CustomGlobalException e) {
+                log.warn(e.getMessage());
+            }
         }
         Thread.sleep(3000);
         Long count = this.waitingTokenJpaRepository.count();
