3afcc26cc4cdcfd7383a6cae59c486f352bcc16c
KrongDev <leegeondev@gmail.com>
2024-08-05 20:55:59 +0900

    fix: Reservation 조회 cache적용 제거 (사유: 단일 유저에 대한 정보로 메모리를 많이 사용할 경우 또다른 사이드 이펙트 발행 으로 인해 캐시 적용 부적격 판단)

--- src/main/java/com/hhplusconcert/application/reservation/facade/ReservationSeekFacade.java
+++ src/main/java/com/hhplusconcert/application/reservation/facade/ReservationSeekFacade.java
@@ -3,7 +3,6 @@ package com.hhplusconcert.application.reservation.facade;
 import com.hhplusconcert.domain.reservation.model.Reservation;
 import com.hhplusconcert.domain.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,13 +13,11 @@ public class ReservationSeekFacade {
     //
     private final ReservationService reservationService;
 
-    @Cacheable(cacheNames = "reservation", key="#reservationId", cacheManager = "cacheManager")
     public Reservation loadReservation(String reservationId) {
         //
         return this.reservationService.loadReservation(reservationId);
     }
 
-    @Cacheable(cacheNames = "reservation", key = "#userId", cacheManager = "cacheManager")
     public List<Reservation> loadReservationsByUserId(String userId) {
         //
         return this.reservationService.loadAllReservationsByUserId(userId);
