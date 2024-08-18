051a1ed1fd37be820cd0a97a8d388b0000154348
KrongDev <leegeondev@gmail.com>
2024-08-18 12:41:25 +0900

    refactor: 불필요한 Transaction 제거

--- src/main/java/com/hhplusconcert/domain/payment/stream/PaymentEventMessageListener.java
+++ src/main/java/com/hhplusconcert/domain/payment/stream/PaymentEventMessageListener.java
@@ -5,8 +5,6 @@ import com.hhplusconcert.domain.payment.event.PaymentConfirmed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
@@ -17,7 +15,6 @@ public class PaymentEventMessageListener {
     private final KafkaProducerCluster kafkaProducerCluster;
 
     @TransactionalEventListener
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void sendMessageHandler(PaymentConfirmed event) {
         //
         this.kafkaProducerCluster.sendMessage(event);
--- src/main/java/com/hhplusconcert/domain/point/stream/PointEventMessageListener.java
+++ src/main/java/com/hhplusconcert/domain/point/stream/PointEventMessageListener.java
@@ -6,8 +6,6 @@ import com.hhplusconcert.domain.point.event.UsedPoint;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
@@ -17,14 +15,12 @@ public class PointEventMessageListener {
     private final KafkaProducerCluster kafkaProducerCluster;
 
     @TransactionalEventListener
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void sendMessageHandler(ChargedPoint event) {
         //
         this.kafkaProducerCluster.sendMessage(event);
     }
 
     @TransactionalEventListener
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void sendMessageHandler(UsedPoint event) {
         //
         this.kafkaProducerCluster.sendMessage(event);
